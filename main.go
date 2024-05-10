633e8bdc022958df7ac0bcd044da6c28697ee9c5
jianzhihuang <55472236+jianzhihuang@users.noreply.github.com>
2024-05-10 03:41:33 +0000

    refactor: Update router handler function name and remove unused code

--- main.go
+++ main.go
@@ -8,12 +8,5 @@ func main() {
 	// 设置静态文件目录
 	r.Static("/", "./public")
 
-	// 其他路由和处理器
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
